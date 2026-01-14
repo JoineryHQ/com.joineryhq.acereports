2af811d9991c5f21d9c569eb4fb671893a19693b
Allen Shaw <allen@JoineryHQ.com>
2026-01-14 11:19:42 -0600

    Minor bug fixes (and code cleanup) on AnnualTaxReceipts report.

--- CRM/Acereports/Form/Report/AnnualTaxReceipts.php
+++ CRM/Acereports/Form/Report/AnnualTaxReceipts.php
@@ -4,23 +4,50 @@ declare(strict_types = 1);
 use CRM_Acereports_ExtensionUtil as E;
 
 class CRM_Acereports_Form_Report_AnnualTaxReceipts extends CRM_Report_Form {
-  
+
   // Live data, TBD.
   //  private $_customDataTransactionalData_fieldIdLetterGroup = 32;
   //  private $_customDataTransactionalData_valuesTall = "TALL', 'FOAL'";
   //  private $_customDataTransactionalData_valuesTpg = 'TPG';
   //  private $_customDataTransactionalData_tableName = '';
   //  private $_customDataTransactionalData_column = '';
-  //  
+  //
   // Dev data, to be replaced with live data, above.
+
+  /**
+   * Custom field id for relevant custom field.
+   */
   private $_customDataTransactionalData_fieldIdLetterGroup = 7;
+
+  /**
+   * "TALL/FOAL" values in that custom field. String for use in mysql IN();
+   */
   private $_customDataTransactionalData_valuesTall = '1, 2';
+
+  /**
+   * "TPG" values in that custom field. String for use in mysql IN();
+   */
   private $_customDataTransactionalData_valuesTpg = 3;
+
+  /**
+   * Custom-group table name for relevant custom field (auto-populated via api in __construct() )
+   */
   private $_customDataTransactionalData_tableName = '';
+
+  /**
+   * Custom-field column name for relevant custom field (auto-populated via api in __construct() )
+   */
   private $_customDataTransactionalData_column = '';
 
+  /**
+   * Fallback value for 'where' value on 'receive_date' field; suitable for use
+   * in the same places as the return value of $this->generateFilterClause().
+   * We need this because we're inserting this value in from(), but it may sometimes
+   * not be specifically un-set by the user in Filters, in which case -- without this --
+   * we'd get an sql syntax error for a where clause like "and [blank]"
+   */
   private $_fallbackDateWhereClause = "1 /* _generateReceiveDateWhereClause */";
-  
+
   protected $_addressField = FALSE;
 
   protected $_emailField = FALSE;
@@ -28,23 +55,22 @@ class CRM_Acereports_Form_Report_AnnualTaxReceipts extends CRM_Report_Form {
   protected $_summary = NULL;
 
   protected $_customGroupExtends = array('Contact', 'Individual', 'Organization');
-  
+
   protected $_customGroupGroupBy = FALSE;
-  
-  function __construct() {
+
+  public function __construct() {
     $customField = \Civi\Api4\CustomField::get(TRUE)
       ->addWhere('id', '=', $this->_customDataTransactionalData_fieldIdLetterGroup)
       ->setLimit(1)
       ->addChain('custom_group', \Civi\Api4\CustomGroup::get(TRUE)
         ->addWhere('id', '=', '$custom_group_id')
-        ->setUseCache(false),
+        ->setUseCache(FALSE),
       0)
       ->execute()
       ->first();
     $this->_customDataTransactionalData_column = $customField['column_name'];
     $this->_customDataTransactionalData_tableName = $customField['custom_group']['table_name'];
-    
-    
+
     $this->_columns = array(
       'civicrm_contact' => array(
         'dao' => 'CRM_Contact_DAO_Contact',
@@ -147,116 +173,93 @@ class CRM_Acereports_Form_Report_AnnualTaxReceipts extends CRM_Report_Form {
     $this->_groupFilter = TRUE;
     $this->_tagFilter = TRUE;
     parent::__construct();
-    
+
     // We've specifically defined a default-enabled 'contact id' column, so we don't need this one,
     // which parent::_contstruct() seems to have added on its own.
     unset($this->_columns['civicrm_contact']['fields']['exposed_id']);
   }
 
-
-  /** 
+  /**
    * Calculate (only once per run) the where clause for the 'receive_date' filter.
    * (This is re-used in a few places, esp in from().)
-   * 
+   *
    * @staticvar String $ret
    * @return String
-   */  
-  function _generateReceiveDateWhereClause() {
+   */
+  private function _generateReceiveDateWhereClause() {
     static $ret;
     if (empty($ret)) {
       $clause = $this->generateFilterClause($this->_columns['civicrm_contribution']['filters']['receive_date'], 'receive_date');
       if (empty($clause)) {
         $ret = $this->_fallbackDateWhereClause;
       }
+      else {
+        $ret = $clause;
+      }
     }
     return $ret;
   }
 
-  function from() {
+  public function from() {
     $this->_from = NULL;
-          
+
     $receiveDateWhereClause = $this->_generateReceiveDateWhereClause();
-    
+
     $this->_from = "
         FROM  civicrm_contact {$this->_aliases['civicrm_contact']} {$this->_aclFrom}
           INNER JOIN civicrm_contribution {$this->_aliases['civicrm_contribution']}
             ON {$this->_aliases['civicrm_contact']}.id =
                {$this->_aliases['civicrm_contribution']}.contact_id AND {$this->_aliases['civicrm_contribution']}.is_test = 0
-        inner join {$this->_customDataTransactionalData_tableName} di 
-          on di.entity_id = {$this->_aliases['civicrm_contribution']}.id
-          and (di.{$this->_customDataTransactionalData_column} in ({$this->_customDataTransactionalData_valuesTall}) or ifnull(di.{$this->_customDataTransactionalData_column}, '') = '')
-        left join (
-          select {$this->_aliases['civicrm_contribution']}.contact_id, sum({$this->_aliases['civicrm_contribution']}.total_amount) as tpg_sum
-          from civicrm_contribution {$this->_aliases['civicrm_contribution']}
-            inner join {$this->_customDataTransactionalData_tableName} di
-              on di.entity_id = {$this->_aliases['civicrm_contribution']}.id
-              and (di.{$this->_customDataTransactionalData_column} in ({$this->_customDataTransactionalData_valuesTpg}))
-          where
-            {$this->_aliases['civicrm_contribution']}.is_test = 0
-            and $receiveDateWhereClause
-          group by {$this->_aliases['civicrm_contribution']}.contact_id
-        ) t1 on t1.contact_id = {$this->_aliases['civicrm_contact']}.id
-        left join (
-          select {$this->_aliases['civicrm_contribution']}.contact_id, sum({$this->_aliases['civicrm_contribution']}.total_amount) as both_sum
-          from civicrm_contribution {$this->_aliases['civicrm_contribution']}
-            inner join {$this->_customDataTransactionalData_tableName} di
-              on di.entity_id = {$this->_aliases['civicrm_contribution']}.id
-              and (
-                (di.{$this->_customDataTransactionalData_column} in ({$this->_customDataTransactionalData_valuesTall}) or ifnull(di.{$this->_customDataTransactionalData_column}, '') = '')
-                or (di.{$this->_customDataTransactionalData_column} in ({$this->_customDataTransactionalData_valuesTpg}))
-              )
-          where
-            {$this->_aliases['civicrm_contribution']}.is_test = 0
-            and $receiveDateWhereClause
-          group by {$this->_aliases['civicrm_contribution']}.contact_id
-        ) t2 on t2.contact_id = {$this->_aliases['civicrm_contact']}.id
-        left join (
-          select {$this->_aliases['civicrm_contribution']}.contact_id, sum({$this->_aliases['civicrm_contribution']}.total_amount) as total_sum
-          from civicrm_contribution {$this->_aliases['civicrm_contribution']}
-            inner join {$this->_customDataTransactionalData_tableName} di
-              on di.entity_id = {$this->_aliases['civicrm_contribution']}.id
-          where
-            {$this->_aliases['civicrm_contribution']}.is_test = 0
-            and $receiveDateWhereClause
-          group by {$this->_aliases['civicrm_contribution']}.contact_id
-        ) t3 on t3.contact_id = {$this->_aliases['civicrm_contact']}.id
-    ";
+          inner join {$this->_customDataTransactionalData_tableName} di
+            on di.entity_id = {$this->_aliases['civicrm_contribution']}.id
+            and (di.{$this->_customDataTransactionalData_column} in ({$this->_customDataTransactionalData_valuesTall}) or ifnull(di.{$this->_customDataTransactionalData_column}, '') = '')
+
+          /* sub-query to get totals (for all contacts) for 'TPG' contributions within the given period */
+          left join (
+            select {$this->_aliases['civicrm_contribution']}.contact_id, sum({$this->_aliases['civicrm_contribution']}.total_amount) as tpg_sum
+            from civicrm_contribution {$this->_aliases['civicrm_contribution']}
+              inner join {$this->_customDataTransactionalData_tableName} di
+                on di.entity_id = {$this->_aliases['civicrm_contribution']}.id
+                and (di.{$this->_customDataTransactionalData_column} in ({$this->_customDataTransactionalData_valuesTpg}))
+            where
+              {$this->_aliases['civicrm_contribution']}.is_test = 0
+              and $receiveDateWhereClause
+            group by {$this->_aliases['civicrm_contribution']}.contact_id
+          ) t1 on t1.contact_id = {$this->_aliases['civicrm_contact']}.id
+
+          /* sub-query to get totals (for all contacts) for 'TPG and TALL/FOAL/NULL' contributions within the given period */
+          left join (
+            select {$this->_aliases['civicrm_contribution']}.contact_id, sum({$this->_aliases['civicrm_contribution']}.total_amount) as both_sum
+            from civicrm_contribution {$this->_aliases['civicrm_contribution']}
+              inner join {$this->_customDataTransactionalData_tableName} di
+                on di.entity_id = {$this->_aliases['civicrm_contribution']}.id
+                and (
+                  (di.{$this->_customDataTransactionalData_column} in ({$this->_customDataTransactionalData_valuesTall}) or ifnull(di.{$this->_customDataTransactionalData_column}, '') = '')
+                  or (di.{$this->_customDataTransactionalData_column} in ({$this->_customDataTransactionalData_valuesTpg}))
+                )
+            where
+              {$this->_aliases['civicrm_contribution']}.is_test = 0
+              and $receiveDateWhereClause
+            group by {$this->_aliases['civicrm_contribution']}.contact_id
+          ) t2 on t2.contact_id = {$this->_aliases['civicrm_contact']}.id
+
+          /* sub-query to get totals (for all contacts) for ALL contributions within the given period */
+          left join (
+            select {$this->_aliases['civicrm_contribution']}.contact_id, sum({$this->_aliases['civicrm_contribution']}.total_amount) as total_sum
+            from civicrm_contribution {$this->_aliases['civicrm_contribution']}
+              inner join {$this->_customDataTransactionalData_tableName} di
+                on di.entity_id = {$this->_aliases['civicrm_contribution']}.id
+            where
+              {$this->_aliases['civicrm_contribution']}.is_test = 0
+              and $receiveDateWhereClause
+            group by {$this->_aliases['civicrm_contribution']}.contact_id
+          ) t3 on t3.contact_id = {$this->_aliases['civicrm_contact']}.id
+      ";
     $this->joinAddressFromContact();
     $this->joinEmailFromContact();
   }
 
-  /**
-   * Add field specific select alterations.
-   *
-   * @param string $tableName
-   * @param string $tableKey
-   * @param string $fieldName
-   * @param array $field
-   *
-   * @return string
-   */
-  function selectClause(&$tableName, $tableKey, &$fieldName, &$field) {
-    return parent::selectClause($tableName, $tableKey, $fieldName, $field);
-  }
-
-  /**
-   * Add field specific where alterations.
-   *
-   * This can be overridden in reports for special treatment of a field
-   *
-   * @param array $field Field specifications
-   * @param string $op Query operator (not an exact match to sql)
-   * @param mixed $value
-   * @param float $min
-   * @param float $max
-   *
-   * @return null|string
-   */
-  public function whereClause(&$field, $op, $value, $min, $max) {
-    return parent::whereClause($field, $op, $value, $min, $max);
-  }
-
-  function alterDisplay(&$rows) {
+  public function alterDisplay(&$rows) {
     // custom code to alter rows
     $entryFound = FALSE;
     $checkList = array();
@@ -279,13 +282,6 @@ class CRM_Acereports_Form_Report_AnnualTaxReceipts extends CRM_Report_Form {
           }
         }
       }
-
-//      if (array_key_exists('civicrm_membership_membership_type_id', $row)) {
-//        if ($value = $row['civicrm_membership_membership_type_id']) {
-//          $rows[$rowNum]['civicrm_membership_membership_type_id'] = CRM_Member_PseudoConstant::membershipType($value, FALSE);
-//        }
-//        $entryFound = TRUE;
-//      }
 
       if (array_key_exists('civicrm_address_state_province_id', $row)) {
         if ($value = $row['civicrm_address_state_province_id']) {
@@ -319,18 +315,21 @@ class CRM_Acereports_Form_Report_AnnualTaxReceipts extends CRM_Report_Form {
       }
     }
   }
-  
-  function statistics(&$rows) {
+
+  /**
+   * Append our own explanatory rows to the 'statistics/used-filters' block.
+   *
+   * @param Array $rows
+   * @return Array
+   */
+  public function statistics(&$rows) {
     $statistics = parent::statistics($rows);
-    
-    $a_params = $this->_params;
-    
+
     if ($this->_generateReceiveDateWhereClause() == $this->_fallbackDateWhereClause) {
       $statistics['filters'][] = [
         'title' => $this->_columns['civicrm_contribution']['filters']['receive_date']['title'],
         'value' => '[No date range specified]',
       ];
-      
     }
 
     $statistics['filters'][] = [
@@ -340,13 +339,12 @@ class CRM_Acereports_Form_Report_AnnualTaxReceipts extends CRM_Report_Form {
     $statistics['filters'][] = [
       'title' => 'About "Sum" columns',
       'value' => '"Sum" columns reflect contribution totals within the "Relevant Contribution Date" range.',
-//      'value' => CRM_Utils_String::htmlToText('foo'),
     ];
-    
+
     return $statistics;
   }
-  
-  function groupBy() {
+
+  public function groupBy() {
     // This report template offers no sorting options, so there's no way for the user
     // to configure a 'group by' setting. Therefore we don't need parent::groupBy() at
     // all, and can simply force 'group by civicrm_contact.id'
